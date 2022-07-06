1cdb2fefbaf2789848f62e1860326a45d45aa651
Stardagger <bumblecade@gmail.com>
2022-07-06 09:12:58 -0700

    addslogoutbutton

--- src/UI/src/shared/layouts/header-layout.js
+++ src/UI/src/shared/layouts/header-layout.js
@@ -1,17 +1,27 @@
-import {inject} from 'aurelia-dependency-injection';
-import {bindable, bindingMode} from 'aurelia-framework';
-import {SharedState} from '../state/shared-state';
+import {inject } from 'aurelia-dependency-injection';
+import {bindable, bindingMode } from 'aurelia-framework';
+import {Router } from 'aurelia-router';
+import {UserService } from '../../shared/services/user-service';
+import {SharedState } from '../state/shared-state';
 
-@inject(SharedState)
+@inject(UserService, SharedState, Router)
 export class HeaderLayout {
   activeRoute = '';
   @bindable({defaultBindingMode: bindingMode.twoWay}) routerConfig;
 
-  constructor(sharedState) {
+  constructor(userService, sharedState, router) {
+    this.userService = userService;
     this.sharedState = sharedState;
+    this.router = router;
   }
 
   routerConfigChanged(newValue, oldValue) {
     this.activeRoute = newValue.name;
   }
+
+  logout() {
+    this.userService.purgeAuth();
+    this.router.navigateToRoute('home');
+  }
 }
+
