88e4d07de6aaca051207afbd1d130779838aede8
Stardagger <bumblecade@gmail.com>
2022-07-10 10:43:46 -0700

    Cleaned up code

--- src/UI/src/shared/layouts/header-layout.js
+++ src/UI/src/shared/layouts/header-layout.js
@@ -1,8 +1,8 @@
-import {inject } from 'aurelia-dependency-injection';
+import {inject} from 'aurelia-dependency-injection';
 import {bindable, bindingMode } from 'aurelia-framework';
-import {Router } from 'aurelia-router';
-import {UserService } from '../../shared/services/user-service';
-import {SharedState } from '../state/shared-state';
+import {Router} from 'aurelia-router';
+import {UserService} from '../../shared/services/user-service';
+import {SharedState} from '../state/shared-state';
 
 @inject(UserService, SharedState, Router)
 export class HeaderLayout {
